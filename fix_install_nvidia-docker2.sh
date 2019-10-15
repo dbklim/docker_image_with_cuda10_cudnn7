ab14f2661cca2c24227b73d07153fd4f31888ed6
Desklop <vladsklim@gmail.com>
2019-10-15 20:29:29 +0300

    Minor bug fixes

--- fix_install_nvidia-docker2.sh
+++ fix_install_nvidia-docker2.sh
@@ -8,7 +8,7 @@
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 apt-key fingerprint 0EBFCD88
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get update
-apt-get install docker-ce
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
+apt-get -y update
+apt-get -y install docker-ce
 service docker restart
